ae31cda3fb25c49a9baefbe52505cf812d96fd38
m-marini <marco.marini@mmarini.org>
2023-10-04 22:33:41 +0200

    Issue #12: Wrong initial status, added more charts

--- octave/main.m
+++ octave/main.m
@@ -3,12 +3,44 @@ clear all;
 file = "../kpis.csv"
 popIdx = 1;
 techIdx = 2;
-foodDeathsTdx = 7
+deathsOIdx = 3;
+deathsSIdx = 7;
+deathsHIdx = 19;
+birthsIdx = 8;
 kfIdx = 9;
 lambdaSIdx = 12;
+keIdx = 18;
+khIdx = 20;
 
 data = csvread(file,1,0);
 
-plot(1-exp(-data(:,[techIdx])));
-grid on
-grid minor
+rows = 2;
+cols = 2;
+
+subplot(rows, cols, 1);
+plot(data(:,[popIdx]));
+grid on;
+grid minor;
+title "Population";
+
+subplot(rows, cols, 2);
+plot(data(:,[kfIdx, khIdx]));
+grid on;
+grid minor;
+legend("Kf", "Kh");
+title "K*";
+
+subplot(rows, cols, 3);
+plot(data(:,[birthsIdx]));
+grid on;
+grid minor;
+title "Births";
+
+subplot(rows, cols, 4);
+plot(data(:,[deathsOIdx, deathsSIdx, deathsHIdx]));
+grid on;
+grid minor;
+legend("Overpopulation", "Starvation", "Helath");
+title "Deaths";
+
+
